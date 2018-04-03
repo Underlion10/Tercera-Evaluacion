e157588d82d0972a4d798020aba51c3c57bb9da3
lione <lioneldelgado73@gmail.com>
2018-04-03 09:58:26 +0100

    03/04/2018 commit -  funcionando mysql-sqlite

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		AccesoDatos mySQL = new AccesoDatos();
-		ArrayList<HashMap<String, Object>> consulta = mySQL.getAllRecords("localhost", "empresa", "root", "", "empleados");
+//		//ArrayList<HashMap<String, Object>> consulta = mySQL.getAllRecords("localhost", "empresa", "root", "", "empleados");
+//		ArrayList<ArrayList<Object>> consulta2 = mySQL.getAllRecords2("localhost", "empresa", "root", "", "empleados");
+		mySQL.getAllRecordsSQLite("C:\\sqlite\\", "test.db", "root", "", "test2");
+		System.out.println("fin");
 	}
 }
--- src/modelo/dao/AccesoDatos.java
+++ src/modelo/dao/AccesoDatos.java
@@ -13,7 +13,7 @@ public class AccesoDatos {
 	
 	private Connection connect = null;
 	
-	public Connection crearConexion(String dominio, String bd, String usr, String clave) {
+	public Connection crearConexionMySQL(String dominio, String bd, String usr, String clave) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql://" + dominio + "/"+ bd;
@@ -27,11 +27,26 @@ public class AccesoDatos {
 		return connect;
 	}
 	
+	public Connection crearConexionSQLite(String dominio, String bd, String usr, String clave) {
+		Connection connect = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			String url = "jdbc:sqlite://" + dominio + "/"+ bd;
+			connect = DriverManager
+			          .getConnection(url, usr, clave);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connect;
+	}
+	
 	public ArrayList<HashMap<String, Object>> getAllRecords(String dominio, String bd, String usr, String clave, String tabla) {
 		//Conexión a la base de datos
+		ArrayList<HashMap<String, Object>> resultados = new ArrayList<HashMap<String, Object>>();
 		try {
-			ArrayList<HashMap<String, Object>> resultados = new ArrayList<HashMap<String, Object>>();
-			Connection connection = this.crearConexion(dominio, bd, usr, clave);
+			Connection connection = this.crearConexionMySQL(dominio, bd, usr, clave);
 			String sql = "SELECT * FROM " + tabla;
 			Statement stm = connection.createStatement();
 			ResultSet rs = stm.executeQuery(sql);
@@ -43,11 +58,60 @@ public class AccesoDatos {
 					}
 					resultados.add(filas);
 			}
-			System.out.println(resultados);
-			return resultados;
+			System.out.println(resultados);			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return resultados;
+	}
+	
+	public ArrayList<ArrayList<Object>> getAllRecords2(String dominio, String bd, String usr, String clave, String tabla) {
+		//Conexión a la base de datos
+		ArrayList<ArrayList<Object>> resultados = new ArrayList<ArrayList<Object>>();
+		try {
+			Connection connection = this.crearConexionMySQL(dominio, bd, usr, clave);
+			String sql = "SELECT * FROM " + tabla;
+			Statement stm = connection.createStatement();
+			ResultSet rs = stm.executeQuery(sql);
+			ResultSetMetaData metaData = rs.getMetaData();
+			ArrayList<Object> columnNames = new ArrayList<Object>();
+			for(int j = 1; j <= metaData.getColumnCount(); j++)
+				columnNames.add(metaData.getColumnName(j));
+			resultados.add(columnNames);
+			while(rs.next()) {
+				ArrayList<Object> filas = new ArrayList<Object>();
+				for(int i = 1; i <= metaData.getColumnCount(); i++) {
+					filas.add(rs.getObject(i));
+				}
+				resultados.add(filas);
+			}
+			System.out.println(resultados);			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultados;
+	}
+	
+	public ArrayList<HashMap<String, Object>> getAllRecordsSQLite(String dominio, String bd, String usr, String clave, String tabla) {
+		//Conexión a la base de datos
+		ArrayList<HashMap<String, Object>> resultados = new ArrayList<HashMap<String, Object>>();
+		try {
+			Connection connection = this.crearConexionSQLite(dominio, bd, usr, clave);
+			String sql = "SELECT * FROM " + tabla;
+			Statement stm = connection.createStatement();
+			ResultSet rs = stm.executeQuery(sql);
+			ResultSetMetaData metaData = rs.getMetaData();
+			while(rs.next()) {
+				HashMap<String, Object> filas = new HashMap<String, Object>();
+					for(int i = 1; i <= metaData.getColumnCount(); i++) {
+						filas.put(metaData.getColumnName(i), rs.getObject(i));
+					}
+					resultados.add(filas);
+			}
+			System.out.println(resultados);			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultados;
 	}
 }
