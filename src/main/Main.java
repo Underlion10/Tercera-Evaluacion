5c72cde125d013d4f58013948906e3e5ae345f61
lione <lioneldelgado73@gmail.com>
2018-04-09 12:05:44 +0100

    3 ev - 09/04/2018 - Conexion MySQL, conexion SQLite y conexion Postgre

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,9 +9,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		AccesoDatos mySQL = new AccesoDatos();
-//		//ArrayList<HashMap<String, Object>> consulta = mySQL.getAllRecords("localhost", "empresa", "root", "", "empleados");
-//		ArrayList<ArrayList<Object>> consulta2 = mySQL.getAllRecords2("localhost", "empresa", "root", "", "empleados");
+		ArrayList<HashMap<String, Object>> consulta = mySQL.getAllRecords("localhost", "personas", "root", "", "persona");
+		ArrayList<ArrayList<Object>> consulta2 = mySQL.getAllRecords2("localhost", "empresa", "root", "", "empleados");
 		mySQL.getAllRecordsSQLite("C:\\sqlite\\", "test.db", "root", "", "test2");
+		String sql = "INSERT INTO persona (fechaNacimiento) values (\"2005-03-12\")";
+		//mySQL.updateRecords("localhost", "personas", "root", "", sql);
+		ArrayList<HashMap<String, Object>> consultaPostgre = mySQL.getAllRecordsPostgreSQL("localhost:5433", "Banco", "postgres", "postgresql", "cajas");
 		System.out.println("fin");
 	}
 }
--- src/modelo/dao/AccesoDatos.java
+++ src/modelo/dao/AccesoDatos.java
@@ -42,14 +42,38 @@ public class AccesoDatos {
 		return connect;
 	}
 	
+	public Connection crearConexionPostgreSQL(String dominio, String bd, String usr, String clave) {
+		Connection connect = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://" + dominio + "/"+ bd;
+			connect = DriverManager
+			          .getConnection(url, usr, clave);
+			System.out.println("He llegado");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connect;
+	}
+	
 	public ArrayList<HashMap<String, Object>> getAllRecords(String dominio, String bd, String usr, String clave, String tabla) {
 		//Conexión a la base de datos
 		ArrayList<HashMap<String, Object>> resultados = new ArrayList<HashMap<String, Object>>();
 		try {
 			Connection connection = this.crearConexionMySQL(dominio, bd, usr, clave);
-			String sql = "SELECT * FROM " + tabla;
+			String sql = "SELECT * FROM " + tabla + " WHERE 1 = 2";
 			Statement stm = connection.createStatement();
 			ResultSet rs = stm.executeQuery(sql);
+			rs.last();
+			if(rs.getRow() < 1) {
+				System.out.println("La tabla está vacia");
+				rs.close();
+				stm.close();
+				return null;
+			}
+			rs.beforeFirst();
 			ResultSetMetaData metaData = rs.getMetaData();
 			while(rs.next()) {
 				HashMap<String, Object> filas = new HashMap<String, Object>();
@@ -113,5 +137,39 @@ public class AccesoDatos {
 			e.printStackTrace();
 		}
 		return resultados;
+	}
+	
+	public ArrayList<HashMap<String, Object>> getAllRecordsPostgreSQL(String dominio, String bd, String usr, String clave, String tabla) {
+		//Conexión a la base de datos
+		ArrayList<HashMap<String, Object>> resultados = new ArrayList<HashMap<String, Object>>();
+		try {
+			Connection connection = this.crearConexionPostgreSQL(dominio, bd, usr, clave);
+			String sql = "SELECT * FROM " + tabla;
+			Statement stm = connection.createStatement();
+			ResultSet rs = stm.executeQuery(sql);
+			ResultSetMetaData metaData = rs.getMetaData();
+			while(rs.next()) {
+				HashMap<String, Object> filas = new HashMap<String, Object>();
+					for(int i = 1; i <= metaData.getColumnCount(); i++) {
+						filas.put(metaData.getColumnName(i), rs.getObject(i));
+					}
+					resultados.add(filas);
+			}
+			System.out.println(resultados);			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultados;
+	}
+	
+	public void updateRecords(String dominio, String bd, String usr, String clave, String sql) {
+		Connection connection = this.crearConexionSQLite(dominio, bd, usr, clave);
+		Statement stm;
+		try {
+			stm = connection.createStatement();
+			stm.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
